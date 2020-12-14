a79955312dcca4eb89c5c5e572f7f2fd910236c3
Issa sangare <issabmsangare@hotmail.com>
2020-12-13 21:08:06 -0500

    add some mongoose validations

--- app.js
+++ app.js
@@ -1,46 +1,120 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // we need to require mongoose after installing it
 
-// mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true, useUnifiedTopology: true})
+// we have to connect mongoose to our mongodb server 
+//'mongodb://localhost:27017' => the port where we can get access to our mongodb database server
+// "/fruitsDB" the name of database we would like to connect or create 
+mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true, useUnifiedTopology: true}) 
 
+// inserting some datas;
+// we have to create a schema for our data
 
-// const fruitSchema = new mongoose.Schema({
-//   name: String,
-//   rate: Number,
-//   review: String
-// });
+const fruitSchema = new mongoose.Schema({
+  // by changing property value to an object, we could insert some validations as below
+  name: {
+    type: String,
+    required: [true, "Please check your data entry, no name specified"] 
+  },
+  rate: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  review: String
+});
 
-// const Fruit = mongoose.model("Fruit", fruitSchema);
+// after creating a schema, we use it to create our model
+// the first parameter is the name of collection(always in singular) => here is Fruit
+// and the second parameter is schema, how we want our collection to be structured => fruitSchema
 
-// const fruit = new Fruit({
-//   name: "Apple",
-//   rating: 7,
-//   review: "Pretty solid as a fruit"
-// });
+const Fruit = mongoose.model("Fruit", fruitSchema); 
 
-// fruit.save();
-mongoose.connect('mongodb://localhost:27017/personsDB', { useNewUrlParser: true, useUnifiedTopology: true})
+// now let's create a new fruit document
+const fruit = new Fruit({
+  // name: "Apple",
+  rate: 9,
+  review: "Pretty solid as a fruit"
+});
+
+// now let's save() to save fruit inside four fruits collection inside FruitsDB
+fruit.save();
+
 const personSchema = new mongoose.Schema({
   name: String,
-  age: Number,
+  age: Number
 });
+
 const Person = mongoose.model("Person", personSchema);
+
 const person = new Person({
   name: "John",
-  age: 36,
-  
+  age: 37
 });
 
-person.save();
+// person.save();
 
+// creating more documents and add them into our collections in one go
+const orange = new Fruit({
+  name: "Orange",
+  rate: 6,
+  review: "This orange is so sweet"
+});
+const banana = new Fruit({
+  name: "Banana",
+  rate: 9,
+  review: "I like this banana"
+});
+const pinneapple = new Fruit({
+  name: "Pinneapple",
+  rate: 9,
+  review: " so sweet"
+});
 
-const findDocuments = function(db, callback){
+// inserting many collections in one go
+// Fruit.insertMany([banana, orange, pinneapple], function(err){
+//   if(err){
+//     console.log(err)
+//   } else{
+//     console.log("Successfully saved all the fruits into fruitsDB")
+//   }
+// })
+
+// reading from database witg mongoose
+// use our model which here Fruit and invoke find(). 
+//Find takes a callback function and has two parameters: err and whatever we get back, here we will call fruits
+Fruit.find(function(err, fruits){
   
-  const collection = db.collection('fruits');
+  if(err){
+    console.log(err)
+  }else {
 
-  collection.find({}).toArray(function(err, fruits){
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits);
-    callback(fruits)
-  })
-}
+    mongoose.connection.close() // to close the connection when we are done with find() and therefore we'll need to use conrol c anymore
+    fruits.forEach((fruit) =>{
+      console.log(fruit.name)
+    })
+  }
+})
+
+// Data validation 
+
+
+
+
+
+
+
+
+
+
+// const findDocuments = function(db, callback){
+  
+//   const collection = db.collection('fruits');
+
+//   collection.find({}).toArray(function(err, fruits){
+//     assert.equal(err, null);
+//     console.log("Found the following records");
+//     console.log(fruits);
+//     callback(fruits)
+//   })
+// };
+
+
